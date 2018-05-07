b6294b2f6a84cecb09fc34d412091b52a78765da
Gabriel Castillo <gcastillo@ucdavis.edu>
2018-05-07 16:05:05 -0700

    Added the db object and default path for profile

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+//const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load Profile model
+const Profile = require('../../models/Profile');
+//const User = require('../../models/User');
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile works!"}));
+
+// @route   GET api/profile
+// @desc    return the profile of the logged in user
+// @access  Public
+router.get('/', passport.aucate('jwt', {session: false}), (req, res) => {
+    const errors = {};
+    Profile.findOne({ us: req.user.id })
+        .then( profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
