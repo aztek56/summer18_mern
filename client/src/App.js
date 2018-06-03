c5c2e385cec973deea887ddedfe0a9b41e1b8e81
Gabriel Castillo <gcastillo@ucdavis.edu>
2018-06-02 23:42:51 -0700

    Finished the login procedure by fixing the form and we guarantee that the token is present and valid

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authAction';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -12,24 +15,34 @@ import store from './store';
 
 import './App.css';
 
+// Check for token
+if(localStorage.jwtToken) {
+    // Set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and expiration
+    const decoded = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+}
+
 class App extends Component {
-  render() {
-    return (
-        <Provider store={ store }>
-          <Router>
-            <div className="App">
-              <Navbar/>
-              <Route exact path="/" component={ Landing }/>
-                <div className="container">
-                    <Route exact path="/register" component={ Register }/>
-                    <Route exact path="/login" component={ Login }/>
-                </div>
-              <Footer/>
-            </div>
-          </Router>
-        </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={ store }>
+                <Router>
+                    <div className="App">
+                        <Navbar/>
+                        <Route exact path="/" component={ Landing }/>
+                        <div className="container">
+                            <Route exact path="/register" component={ Register }/>
+                            <Route exact path="/login" component={ Login }/>
+                        </div>
+                        <Footer/>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
