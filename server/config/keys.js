a23a981d2c33df73833e1c961bc60a842067fbe8
Gabriel Castillo <gcastillo@ucdavis.edu>
2018-06-05 22:23:29 -0700

    Made modifications to secure the keys when sharing in a public environment

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI:'mongodb://admin:admin@ds255889.mlab.com:55889/mern_app',
-    secretOrKey: 'secret'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
